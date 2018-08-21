fa686370a94d787eb7df9592ff24045b41178403
Katherine Smith <katherine.lo.smith@gmail.com>
2018-08-21 10:26:56 -0700

    Complete Article.fetchAll function to retrieve articles with AJAX

---
+++ katherine-kris/scripts/article.js
@@ -0,0 +1,64 @@
+'use strict';
+
+function Article (rawDataObj) {
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
+}
+
+// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+Article.all = [];
+
+// COMMENT: Why isn't this method written as an arrow function?
+// We are using the this keyword. Therefore, we want the method to have context.
+Article.prototype.toHtml = function() {
+  let template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+
+  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // Not sure? Check the docs!
+  // The line below checks to see if this.publishedOn exists. If it exists, or it's true, then this.publishStatus becomes `published ${this.daysAgo} days ago`. Otherwise, this.publishedOn does not exist and then this.publishStatus becomes '(draft)'.
+  // The line below is a ternary operator. In a ternary operator, the question mark checks if the previous statement is true so it basically represents an if statement. The colon represents the else of the if statement.
+
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  this.body = marked(this.body);
+
+  return template(this);
+};
+
+// REVIEW: There are some other functions that also relate to all articles across the board, rather than just single instances. Object-oriented programming would call these "class-level" functions, that are relevant to the entire "class" of objects that are Articles.
+
+// REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
+
+// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// This function is called in the Article.fetchAll function if localStorage contains rawData. rawData now represents the JSON file in localStorage. This is different from previous labs because we are now taking the JSON file out of localStorage.
+Article.loadAll = articleData => {
+  articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+
+  articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
+}
+
+// REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
+Article.fetchAll = () => {
+  // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
+  if (localStorage.rawData) {
+    let articleData = JSON.parse(localStorage.getItem("rawData"));
+
+    Article.loadAll(articleData);
+
+  } else {
+
+    $.getJSON('http://127.0.0.1:8080/data/hackerIpsum.json')
+      .then(data => {
+        console.log("Got the data");
+        console.log(data);
+        localStorage.setItem("rawData", JSON.stringify(data));
+      })
+  }
+}
+
+Article.fetchAll();
